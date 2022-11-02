015010e4d41c03e5edf11cd5ec4768f48b958efc
Lucas <lucas.borg@etu.univ-amu.fr>
2022-11-02 17:40:04 +0100

    ebauche de classe pour le 2e probleme

---
+++ src/stable/Stable.java
@@ -0,0 +1,24 @@
+public class Stable {
+    int n ;
+    int k ;
+    int[][] G ;
+
+    public Stable (tab,nb,kp){
+        n = nb ;
+        k = kp ;
+        G = new int[n][] ;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; i < n ; i++){
+                G[i][j] = tab[i][j] ;
+            }
+        }
+    }
+
+    public boolean solve(){
+        System.out.println("Resolution de l'instance") ;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
